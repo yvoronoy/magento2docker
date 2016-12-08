0cd95b75403adbf90b5e4aa6d40951558d238658
Yaroslav Voronoy <yvoronoy@magento.com>
2016-12-08 16:18:03 +0200

    Fixed #2: xDebug should be disabled out of box. XDebug affects the performance.

---
+++ env/misc/xdebug-php.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+VERSION=1.0.0
+SCRIPT_NAME=$(basename "${0}");
+
+################################################################################
+# Action Controllers
+################################################################################
+xdebugAction()
+{
+  local flag="${1:-0}"
+  local ini=$(php --ini | grep -i xdebug | head -1)
+  ini=$(echo "$ini" | sed s/,$//)
+
+  if [ ! -f "${ini}" ]; then
+	  echo "> ERROR: No Xdebug INI file found ..." 1>&2 && return 1
+  fi
+
+  if [ "$flag" -eq 0 ]; then
+      sed -i'' -e 's/^zend/;zend/g' "${ini}" \
+        && echo "> Xdebug DISABLED in ${ini}" \
+        || { echo "> ERROR: failed to update ${ini} file ..." 1>&2 && return 1; }
+  else
+      sed -i'' -e 's/^;zend/zend/g' "${ini}" \
+        && echo "> Xdebug ENABLED in ${ini}" \
+        || { echo "> ERROR: failed to update ${ini} file ..." 1>&2 && return 1; }
+  fi
+
+  apachectl -k graceful
+  php -v
+}
+
+
+################################################################################
+# Main
+################################################################################
+main()
+{
+  xdebugAction "$@"
+}
+main "${@:-}"
+
